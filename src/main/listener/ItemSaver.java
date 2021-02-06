93e32628455bef5e474b29600a3a5ec686d26fc8
Rafael Sundorf <captn.rafaelh@gmail.com>
2021-02-06 23:17:39 +0100

    Checkpoint for today

--- src/main/listener/ItemSaver.java
+++ src/main/listener/ItemSaver.java
@@ -1,20 +1,18 @@
 package main.listener;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.ItemDespawnEvent;
+import org.bukkit.event.player.PlayerItemBreakEvent;
+import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
@@ -23,39 +21,32 @@ import net.md_5.bungee.api.ChatColor;
 
 public class ItemSaver implements Listener {
 	
-	
 	public static List<Inventory> trash_invs = new ArrayList<>();
 	
-	static File file = new File(Main.main.getDataFolder(), "trashitems.yml");
-	static FileConfiguration trashitems = YamlConfiguration.loadConfiguration(file);
-	
 	static {
-		if(!file.exists()) {
-			try {
-				file.createNewFile();
-			} catch (IOException e) {}
+		
+		ConfigurationSection section = Main.main.getConfig().getConfigurationSection("TrashItems");
+		
+		if(section == null) {
+			section = Main.main.getConfig().createSection("TrashItems");
+			section.set("All", new ArrayList<ItemStack>());
 		}
 		
-		ConfigurationSection section = trashitems.getConfigurationSection("TrashItems");
+		@SuppressWarnings("unchecked")
+		List<ItemStack> itemlist = (List<ItemStack>) section.getList("All");
 		
-		if(section==null) {
-			section = trashitems.createSection("TrashItems");
-		}
-		
-		if(!section.getKeys(false).isEmpty()) {
-			int keys = section.getKeys(false).size();
+		if(!itemlist.isEmpty()) {
 			
-			int inv_keys = (int) Math.ceil(keys / 54);
+			int keys = itemlist.size();
+			int inv_counter = 1;
 			
-			Main.main.getLogger().info(inv_keys +  " inv_keys");
-			for(int iv = 0; iv < keys; iv=iv+54) {
+			Main.main.getLogger().info(keys +  " inv_keys");
+			for (int iv = 0; iv < keys; iv++) {
 				Main.main.getLogger().info(iv +  " iv");
-				Inventory inv = Bukkit.createInventory(null, 54, ChatColor.BLUE+"Mülleimer "+ChatColor.DARK_GRAY+"- Seite "+ Math.round(Math.ceil(iv/54)+1));
-				for(int i = 0; i < 54; i++) {
+				Inventory inv = Bukkit.createInventory(null, 54, ChatColor.BLUE + "Mülleimer " + ChatColor.DARK_GRAY + "- Seite " + inv_counter++);
+				for (int i = 0; i < 54 && itemlist.size() < iv+i; i++) {
 					Main.main.getLogger().info(i +  "Item");
-					
-					ItemStack itemstack = section.getItemStack((iv+i)+"");
-					inv.setItem(i, itemstack);
+					inv.setItem(i, itemlist.get(iv+i));
 				}
 				trash_invs.add(inv);
 				
@@ -66,7 +57,7 @@ public class ItemSaver implements Listener {
 		else {
 			Main.main.getLogger().warning("No TrashItems found!");
 			
-			Inventory inv = Bukkit.createInventory(null, 54, ChatColor.BLUE+"Mülleimer "+ChatColor.DARK_GRAY+"- Seite 1");
+			Inventory inv = Bukkit.createInventory(null, 54, ChatColor.BLUE + "Mülleimer " + ChatColor.DARK_GRAY + "- Seite " + 1);
 			trash_invs.add(inv);
 			
 		}
@@ -74,39 +65,44 @@ public class ItemSaver implements Listener {
 	
 	
 	public static void saveTrashItems() {
-		
-		ConfigurationSection section = trashitems.createSection("TrashItems");
+		ConfigurationSection section = Main.main.getConfig().createSection("TrashItems");
 		int offset = 0;
-		for(int iv = 0; iv < trash_invs.size(); iv++) {
-			Main.main.getLogger().warning(iv +  " iv");
-			for(int i = 0; i < 54; i++) {
-				section.set((offset+i)+"", trash_invs.get(iv).getItem(i));
-				Main.main.getLogger().warning(i +  " i §dset" + (offset+i));
-				
+		List<ItemStack> itemlist = new ArrayList<>();
+		for(Inventory inventory : trash_invs) {
+			if (!inventory.isEmpty()) {
+				for (ItemStack item : inventory.getContents()) {
+					if (item != null) {
+						Main.main.getLogger().warning("Not null");
+						itemlist.add(item);
+					}
+					
+				}
+			} else {
+				Main.main.getLogger().warning("Inventory is empty");
 			}
-			offset=offset+54;
+			
 		}
-		
-		try {
-			if(!file.exists()) {
-				file.createNewFile();
-			}
-			trashitems.save(file);
-		} catch (IOException e) {}
+		itemlist.add(new ItemStack(Material.ACACIA_BOAT, 2));
+		itemlist.add(new ItemStack(Material.ACACIA_BOAT, 2));
+		itemlist.add(new ItemStack(Material.ACACIA_BOAT, 2));
+		itemlist.add(new ItemStack(Material.ACACIA_BOAT, 2));
+		itemlist.add(new ItemStack(Material.ACACIA_BOAT, 2));
+		section.set("All", itemlist);
 	}
 	
-	
-	public static void saveItem(ItemStack itemstack) {
-		for(int i=0; i < trash_invs.size(); i++) {
-			//Bukkit.broadcastMessage("Empty: "+trash_invs.get(i).addItem(itemstack).isEmpty());
+
+	public void saveItem(ItemStack itemstack) {
+		Bukkit.broadcastMessage("Saving---");
+		for(int i = 0; i < trash_invs.size(); i++) {
+			Bukkit.broadcastMessage("Empty: " + trash_invs.get(i).addItem(itemstack).isEmpty());
 			
-			if(trash_invs.get(i).addItem(itemstack).isEmpty()) {
+			if (trash_invs.get(i).addItem(itemstack).isEmpty()) {
 				i = trash_invs.size(); // Oder einfach eine größere Zahl
 			}
 			else {
-				if(i+1 == trash_invs.size()) {
-					Inventory inv = Bukkit.createInventory(null, 54, ChatColor.BLUE+"Mülleimer "+ChatColor.DARK_GRAY+"- Seite "+(i+2));
-					trash_invs.add(inv);
+				if(i + 1 == trash_invs.size()) {
+					//Inventory inv = Bukkit.createInventory(null, 54, ChatColor.BLUE + "Mülleimer " + ChatColor.DARK_GRAY + "- Seite " + (i + 2));
+					//trash_invs.add(inv);
 				}
 			}
 		}
@@ -114,37 +110,28 @@ public class ItemSaver implements Listener {
 	
 	@EventHandler
 	public void onItemDespawn(ItemDespawnEvent e) {
-		if(!e.isCancelled() && e.getEntity().getPickupDelay() == 0) {
+		
+		
+		if(!e.isCancelled()) {
 			saveItem(e.getEntity().getItemStack());
 			Bukkit.broadcastMessage(
 			"§e-- §7" + e.getEntity().getPickupDelay()
 			+ " / " + e.getEntity().getLastDamageCause()
 			+ " / " + e.getEntity().getOwner()
-			+ " / " + e.getEntity().getItemStack().getAmount()
-			+ " / " + e.getEntity().getThrower() + "\nItemDespawn §e--" + e.isCancelled());
+			+ " / " + e.getEntity().getThrower() + "\nItemDespawn §e--" + e.isCancelled() + "c");
 		}
 	}
 	
 	@EventHandler(ignoreCancelled = false)
 	public void onItemDeath(EntityDeathEvent e) {
-		//Bukkit.broadcastMessage(e.getEntityType()+" type");
+		Bukkit.broadcastMessage(e.getEntityType()+" type");
 		if(e.getEntityType().equals(EntityType.DROPPED_ITEM)) {
 			saveItem(((Item) e.getEntity()).getItemStack());
 			Bukkit.broadcastMessage("§eItemDeath");
 		}
 	}
 	
-	@EventHandler(ignoreCancelled = false)
-	public void onItemDeath(EntityDamageEvent e) {
-		//Bukkit.broadcastMessage(e.getEntityType()+" type");
-		if(e.getEntityType().equals(EntityType.DROPPED_ITEM)) {
-			//saveItem(((Item) e.getEntity()).getItemStack());
-			
-			Bukkit.broadcastMessage("§eItemDamage §5"+e.getEntity().isDead());
-		}
-	}
-	
-	/*@EventHandler
+	@EventHandler
 	public void onPlayerItemDamage(PlayerItemDamageEvent e) {
 		saveItem(e.getItem());
 		Bukkit.broadcastMessage("§ePlayerItemDamage");
@@ -154,7 +141,7 @@ public class ItemSaver implements Listener {
 	public void onPlayerItemBreak(PlayerItemBreakEvent e) {
 		saveItem(e.getBrokenItem());
 		Bukkit.broadcastMessage("§ePlayerItemBreak");
-	}*/
+	}
 	
 	
 	
